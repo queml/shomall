8533760091a3102631db3f8d25ed61f91bc032d0
queml <2829163426@qq.com>
2021-08-16 15:53:55 +0800

    add

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+   <hello-world/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+export default {
+  name: 'App',
+  components: {HelloWorld}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+  <el-container>
+   <el-header>Header</el-header>
+    <el-main>main</el-main>
+  </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
